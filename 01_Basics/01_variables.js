71814d27a6be55637843f9bcd0d3f4ad5f6afa15
Tarun Sharma <tarunsharma01s@outlook.com>
2026-02-03 17:48:53 +0530

    Variables and Datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 5478540
+let email = "Tarun@Google.com" // Use let &
+var accountPass = "5780" // avoid var's 
+Accountcity = "Jaipur"  // Never use variables directly
+let accountState;
+
+console.log(accountId);
+
+console.table([email, accountPass, Accountcity, accountState])
+
+// accountId = 7567975 // Not allowed (Constant do not change)
+email = "ts.ts@gmail.com"
+accountPass = "4566"
+Accountcity = "Hisar"
+accountState = "Haryana"
+
+console.table([accountId, email, accountPass, Accountcity, accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) -> X  // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+let name = "Tarun"
+let age = 18
+let loggedin = true/false
+let state;
+
+// Data Types
+// numbers = 2 to power 53
+// BigInt = for large integer values
+// String = " "
+// Boolean = true/false 
+// null => standalone value ==> Type -> Object
+// undefined 
+// symbol => unique 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
